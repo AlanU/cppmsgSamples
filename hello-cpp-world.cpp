8b4688dd3a67ae07e9b48d7c0015238aa2221c03
cppmsg <grant.cppmsg.cloud9@emailmeinfo.com>
2016-03-05 02:38:57 +0000

    code from march 4 meetup

--- hello-cpp-world.cpp
+++ hello-cpp-world.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <memory>
 #include "test.h"
+//alan uthoff
+
+
+
 int main() {
+   std::unique_ptr<interface>  iptr(new anotherClass());
+    iptr->foo();
     std::cout << "Hello World!" << std::endl;
 }
+
+
--- inc/test.h
+++ inc/test.h
@@ -1,7 +1,35 @@
 #pragma once
 
 
-class test
+class interface
 {
-    
+    public:
+    virtual void foo() = 0;
+    virtual ~interface(){
+        
+    }
 };
+
+
+class baseClass : public interface
+{
+    public:
+        virtual ~baseClass() {}
+        virtual void foo() override  {
+            std::cout<<"foo"<<std::endl;
+        }
+};
+
+class anotherClass : public baseClass
+{
+    public:
+    virtual ~anotherClass()
+    {
+         std::cout <<"anotherClass destroyed " << std::endl;
+    }
+    virtual void foo() override {
+        std::cout <<"anotherClass" << std::endl;
+    }
+};
+
+
